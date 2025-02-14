f18bb1cb5f3d897493f19f144a5b50cf47325009
egasan21 <140286610+egasan21@users.noreply.github.com>
2025-02-14 04:59:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Overview
+
+This app is my personal project that will be used for data entry involving BCBAs workflow. I used the starter template from AWS to build a basis for my project. The stack uses React+Vite and AWS Amplify. Implements user authentication, API, and database capabilities to store previous data and to compare with as well.
+
+"This template equips you with a foundational React application integrated with AWS Amplify, streamlined for scalability and performance. It is ideal for developers looking to jumpstart their project with pre-configured AWS services like Cognito, AppSync, and DynamoDB."
+
+## Features
+
+- **Authentication**: Setup with Amazon Cognito for secure user authentication.
+- **API**: Ready-to-use GraphQL endpoint with AWS AppSync.
+- **Database**: Real-time database powered by Amazon DynamoDB.
+
+
+## Security
+
+See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+
+## License
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
+
+function App() {
+  const { user, signOut } = useAuthenticator();
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+
+  return (
+    <main>
+      <button onClick={signOut}>Sign out</button>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li 
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 Starting template for a Fitness webapp.
+        <br />
+        <a href="https://github.com/htmw/2025SA-Team2">
+          Please visit our GitHub page. Thank you.
+        </a>
+      </div>
+    </main>
+  );
+}
+
+export default App;
